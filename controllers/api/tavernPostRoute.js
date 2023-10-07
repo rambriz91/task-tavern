e922f5cfadfb2e9517973533e44081875396d212
Jorge Gonzalez <jorge.1014@gmail.com>
2023-10-07 12:33:36 -0700

    added delete route for quest posts

--- controllers/api/tavernPostRoute.js
+++ controllers/api/tavernPostRoute.js
@@ -34,3 +34,23 @@ router.put('id:', withAuth, async (req, res) => {
     }
 });
 
+router.delete('/:id', withAuth, async (req, res) => {
+    console.log(req.params.id);
+    try {
+        const questPostData = await Quest.destroy({
+            where: {
+                id: req.params.id,
+            },
+        });
+        if (!questPostData) {
+            res.status(404).json({ message: 'No Quest found with this id!' });
+            return;
+        }
+        res.status(200).json(questPostData);
+    } catch (err) {
+        res.status(500).json(err);
+    }
+});
+
+module.exports = router;
+
