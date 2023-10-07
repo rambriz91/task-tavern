0438801d6e2228d744f81038a327bc104b6e2491
Jorge Gonzalez <jorge.1014@gmail.com>
2023-10-07 13:30:26 -0700

    updated seed.js

--- seeds/seed.js
+++ seeds/seed.js
@@ -7,10 +7,17 @@ const questPostData = require('./questPostData.json');
 const seedDatabase = async () => {
   await sequelize.sync({ force: true });
 
-  await User.bulkCreate(userData, {
+  const users = await User.bulkCreate(userData, {
     individualHooks: true,
     returning: true,
   });
+
+  for (const Quest of questPostData) {
+    await Quest.create({
+      ...Quest,
+      user_id: users[Math.floor(Math.random() * users.length)].id,
+    });
+  }
 
   process.exit(0);
 };
