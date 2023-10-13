82bf8283c5320d3e918967988682037262d9cfa8
rambriz91 <rambriz91@gmail.com>
2023-10-12 18:46:54 -0700

    added functionality to the post quest view.

---
+++ public/js/post-quest.js
@@ -0,0 +1,25 @@
+const postFormHandler = async (event) => {
+  event.preventDefault();
+
+  const title = document.querySelector('#title-input').value.trim();
+  const description = document.querySelector('#dsc-input').value.trim();
+  const reward = document.querySelector('#rwd-input').value.trim();
+  const quest_type = document.querySelector('#qst-type').value.trim();
+
+  if (title && description && reward && quest_type) {
+    const response = await fetch('/api/tavernpost', {
+      method: 'POST',
+      body: JSON.stringify({ title, description, reward, quest_type }),
+      headers: { 'Content-Type': 'application/json' },
+    });
+
+    if (response.ok) {
+      document.location.replace('/');
+    } else {
+      alert(response.statusText);
+    }
+  }
+};
+document
+  .querySelector('#post-quest-form')
+  .addEventListener('submit', postFormHandler);
