ea2bf80e75b084905c2b843e24a84364ad96dd88
rambriz91 <rambriz91@gmail.com>
2023-10-16 12:24:04 -0700

    poster information now appears in the Taverboard and Individual views.

--- models/index.js
+++ models/index.js
@@ -11,21 +11,26 @@ User.hasMany(Badge, {
   foreignKey: 'user_id',
   onDelete: 'CASCADE',
 });
-
-Quest.belongsToMany(User, {
-  through: {
-    model: UserQuest,
-    unique: false,
-    foreignKey: 'quest_id',
-  },
+Quest.belongsTo(User, {
+  foreignKey: 'poster_id',
 });
-
-User.belongsToMany(Quest, {
-  through: {
-    model: UserQuest,
-    unique: false,
-    foreignKey: 'user_id',
-  },
+User.hasMany(Quest, {
+  foreignKey: 'poster_id',
 });
+// Quest.belongsToMany(User, {
+//   through: {
+//     model: UserQuest,
+//     unique: false,
+//     foreignKey: 'quest_id',
+//   },
+// });
+
+// User.belongsToMany(Quest, {
+//   through: {
+//     model: UserQuest,
+//     unique: false,
+//     foreignKey: 'user_id',
+//   },
+// });
 
 module.exports = { User, Badge, Quest, UserQuest };
--- seeds/seed.js
+++ seeds/seed.js
@@ -1,9 +1,10 @@
 const sequelize = require('../config/connection');
-const { User, Quest, Badge } = require('../models');
+const { User, Quest, Badge, UserQuest } = require('../models');
 
 const userData = require('./userData.json');
 const questPostData = require('./questPostData.json');
 const badgeData = require('./badgeData.json');
+const userQuestData = require('./userQuestData');
 
 const seedDatabase = async () => {
   await sequelize.sync({ force: true });
@@ -16,6 +17,8 @@ const seedDatabase = async () => {
   const quests = await Quest.bulkCreate(questPostData);
 
   const badges = await Badge.bulkCreate(badgeData);
+
+  const userQuests = await UserQuest.bulkCreate(userQuestData);
 
   process.exit(0);
 };
