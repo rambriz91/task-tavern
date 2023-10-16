9faae5dab23764f4966186a259e415c5ed481e66
rambriz91 <rambriz91@gmail.com>
2023-10-15 20:01:18 -0700

    removed main.css file, and consolidated the css.

--- models/Quest.js
+++ models/Quest.js
@@ -38,7 +38,7 @@ Quest.init(
     },
     poster_id: {
       type: DataTypes.INTEGER,
-      allowNull: true,
+      allowNull: false,
       references: {
         model: 'user',
         key: 'id',
